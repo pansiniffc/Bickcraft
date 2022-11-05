01041a88e84ccf3d42c0700e869edc20efa991e8
pansiniffc <renanpansini15@gmail.com>
2022-11-04 21:25:05 -0400

    perguntas js

--- js/script.js
+++ js/script.js
@@ -23,3 +23,22 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+//Perguntas Frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const respota = document.getElementById(controls);
+
+  respota.classList.toggle("ativa");
+  const ativa = reposta.classList.contains("ativa");
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
